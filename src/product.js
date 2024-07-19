9aeac8f08eb9e2023b790de99702ce3ff5d6ed0d
RejoiceFiamor <rejoicemawusi57@gmail.com>
2024-07-19 05:06:37 -0700

    added a few changes

---
+++ src/product.js
@@ -0,0 +1,9 @@
+function DropText(){
+    let africa = document.querySelector("#africa");
+    africa.style.width = "auto";
+    africa.style.drotpext ="display";
+    money.style.drotpext ="display";
+   }
+
+let money = document.querySelector("#money");
+money.addEventListener('dblclick', DropText );
--- src/signup.js
+++ src/signup.js
@@ -1,13 +1,13 @@
-const navLinks = document.querySelectorAll(".navigation a");
+// const navLinks = document.querySelectorAll(".navigation");
 
-navLinks.forEach((link) => {
-  link.addEventListener("mouseenter", () => {
-    link.style.width = "300px"; // Increase width on hover 
-    link.style.height = "200px"; // Increase height on hover 
-  });
-
-  link.addEventListener("mouseleave", () => {
-    link.style.width = "auto"; // Reset width on mouse leave
-    link.style.height = "auto"; // Reset height on mouse leave
-  });
-});
+// navLinks.forEach((link) => {
+//   link.addEventListener("mouseenter", () => {
+//     link.style.width = "300px"; // Increase width on hover 
+//     link.style.height = "200px"; // Increase height on hover 
+//   });
+  
+//   link.addEventListener("mouseleave", () => {
+//     link.style.width = "auto"; // Reset width on mouse leave
+//     link.style.height = "auto"; // Reset height on mouse leave
+//   });
+// });
