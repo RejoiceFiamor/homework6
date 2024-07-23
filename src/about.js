d8d8563558c212c6ed66d82e39ef719c026b7383
HP <rejoicemawusi57@gmail.com>
2024-07-23 17:47:57 +0000

    added a lot of changes

--- src/about.js
+++ src/about.js
@@ -7,3 +7,12 @@ function clickToCall() {
 }
 let action = document.getElementById("action");
 action.addEventListener("click", clickToCall);
+
+// adding a link button from about us page to product page
+function BackToProductPage(){
+  let unique = document.getElementById("unique");
+  unique.style.display = "block";
+}
+let product = document.getElementById("RawFoodProduct")
+product.addEventListener("dblclick",BackToProductPage);
+
--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
-//When the user enters the explore button on the 
-//homepage; it takes the users name
+// When the user enters the explore button on the 
+// homepage; it takes the users name
 function promptName(){
     let name = prompt("What is your name?");
     let Africa = document.querySelector("#African");
+    if(name){
     Africa.innerHTML=`👋 Welcome ${name}, To Taste The Raw  Goodness Of African `;
+    } else{
+        Africa.innerHTML=`👋Hi lovely, welcome To Taste The Raw Goodness Of African`;
+    }
     let Goodness = document.getElementById("Goodness");
-    Goodness.innerHTML =`👋 Hi ${name}, Browse Through The African Goodness `;
+    if(name){
+      Goodness.innerHTML = `👋 Hi ${name}, Browse Through The African Goodness `;
+    } else{
+        Goodness.innerHTML=`👋Hi lovely, Browse Through The African Goodness`;
+    }
 }
 
 let display = document.getElementById("display");
 display.addEventListener("mouseenter", promptName);
 
 
-//Added a click eventto the okro img
-function alertokro(){
-    alertokro("https://www.healthline.com/nutrition/okra-health-benefits");
-}
 
-let vegetable = document.querySelector("#okro");
-vegetable.addEventListener("dblclick",alertokro);
-
-// adding click to load more event
-function loadMore(){
-    let lean = document.getElementById("learn"); //element that contain the images
-    lean.style.display="flex";
-}
-let leann = document.getElementById("leann");
-leann.addEventListener("click", loadMore);
 
 
